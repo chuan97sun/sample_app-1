f3eb2dd5903da761da0a8d2daed25471c1e93899
stephanie1997-kek <miner1997@graduate.utm.my>
2020-09-01 12:15:39 +0800

    Chapter_9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: :show
 
-  def show
-  	@user = User.find_by id: params[:id]
-  end
+  def show; end
 
   def new
     @user = User.new
